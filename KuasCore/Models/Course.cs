aa29b7ab94fd1cd961a3e9d8de8aaf6bde3def9a
1101137245 <1101137245@gm.kuas.edu.tw>
2015-06-05 14:57:29 +0800

    V1.0

---
+++ KuasCore/Models/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public string Aboard { get; set; }
+
+        public string Getoff { get; set; }
+
+        public string Number { get; set; }
+
+        public string Site { get; set; }
+
+        public string Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Course: Id = " + Id + ", Name = " + Name + ".";
+        }
+    }
+}
