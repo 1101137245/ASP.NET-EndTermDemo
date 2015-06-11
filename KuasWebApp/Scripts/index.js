abe7e155cb9d86766c0ff3a13db99215a9dc4bf5
1101137245 <1101137245@gm.kuas.edu.tw>
2015-06-12 01:13:19 +0800

    timepick by zhi

--- KuasWebApp/Scripts/index.js
+++ KuasWebApp/Scripts/index.js
@@ -96,7 +96,8 @@ function ChangeClass() {
         },
         dataType: 'json',
         success: function (data) {
-            UpdataClass(data)
+            UpdataClass(data);
+            console.log(data);
 
         },
         error: function () {
@@ -107,37 +108,162 @@ function ChangeClass() {
 function UpdataClass(data) {
     var AboardStation = $("#AboardStation").val();
     var GetoffStation = $("#GetoffStation").val();
-    var Class1;
-    var Class2;
-    var Class3;
-    var Class4;
-    var Price;
-    var Number;
+    var Class1, Class2, Class3, Class4, Price, Number;
+    var Class1H = 0, Class1M = 0, Class1T, Class2H = 6, Class2M = 0, Class2T, Class3H = 12, Class3M = 0, Class3T, Class4H = 18, Class4M = 0, Class4T;
     for (var i = 0; i < data.length; i++) {
         if ((data[i].Aboard == AboardStation && data[i].Getoff == GetoffStation) ||
-            (data[i].Getoff == AboardStation && data[i].Aboard == GetoffStation))
-        {
-            Class1 = data[i].Class1;
-            Class2 = data[i].Class2;
-            Class3 = data[i].Class3;
-            Class4 = data[i].Class4;
-            Price = data[i].Price;
+            (data[i].Getoff == AboardStation && data[i].Aboard == GetoffStation)) {
+            console.log(data[i].Class1);
+            Class1 = parseInt(data[i].Class1);
+            Class2 = parseInt(data[i].Class2);
+            Class3 = parseInt(data[i].Class3);
+            Class4 = parseInt(data[i].Class4);
+            Price = parseInt(data[i].Price);
             Number = data[i].Number;
             $("#Number").val(Number);
 
             $("#Price").val(Price);
+            for (var w = 0; w < (360 / Class1) ; w++) {
 
-            $("#select-choice-a-menu").empty();
-     //       $("#select-choice-a-button > *").remove();
-            $('<ul class="ui-selectmenu-list ui-listview" id="select-choice-a-menu" role="listbox" aria-labelledby="select-choice-a-button">\n\
-                <li data-option-index="0" data-icon="false" data-placeholder="true" class="ui-screen-hidden" role="option" aria-selected="false">\n\
-                <a href="#" tabindex="-1" class="ui-btn">00:00 ~ 6:00</a></li>\n\
-                <li data-option-index="1" data-icon="false" class="ui-first-child" role="option" aria-selected="false">\n\
-                <a href="#" tabindex="-1" class="ui-btn">'+Class1+'</a></li>\n\
-                <li data-option-index="2" data-icon="false" class="" role="option" aria-selected="false">\n\
-                <a href="#" tabindex="-1" class="ui-btn">' + Class1 + '</a></li>\n\
-                <li data-option-index="3" data-icon="false" class="ui-last-child" role="option" aria-selected="false">\n\
-                <a href="#" tabindex="-1" class="ui-btn">' + Class1 + '</a></li></ul>').appendTo("#select-choice-a-menu");
+                if (Class1M + Class1 >= 120) {
+                    Class1H++;
+                    Class1M = Class1M + (Class1 - 60);
+                    do {
+                        Class1H++;
+                        Class1M = Class1M - 60;
+                    } while (Class1M >= 60)
+
+                } else if (Class1M + Class1 >= 60 && Class1M + Class1 < 120) {
+                    Class1H++;
+                    Class1M = Class1M + (Class1 - 60);
+                } else {
+                    Class1M = Class1M + Class1;
+                }
+
+                if (Class1H < 10 && Class1M < 10) {
+                    Class1T = "0" + Class1H + ":0" + Class1M;
+                } else {
+                    if (Class1H < 10 && Class1M > 9) {
+                        Class1T = "0" + Class1H + ":" + Class1M;
+                    } if (Class1H > 9 && Class1M < 10) {
+                        Class1T = Class1H + ":0" + Class1M;
+                    } else {
+                        Class1T = Class1H + ":" + Class1M;
+                    }
+                }
+
+                var content = '<option value="' + Class1T + '">' + Class1T + '</option>';
+                $(content).appendTo($("#select-choice-a"));
+
+            }
+            $("#select-choice-a").selectmenu("refresh");
+
+            for (var x = 0; x < (360 / Class2) ; x++) {
+
+                if (Class2M + Class2 >= 120) {
+                    Class2H++;
+                    Class2M = Class2M + (Class2 - 60);
+                    do {
+                        Class2H++;
+                        Class2M = Class2M - 60;
+                    } while (Class2M >= 60)
+
+                } else if (Class2M + Class2 >= 60 && Class2M + Class2 < 120) {
+                    Class2H++;
+                    Class2M = Class2M + (Class2 - 60);
+                } else {
+                    Class2M = Class2M + Class2;
+                }
+
+                if (Class2H < 10 && Class2M < 10) {
+                    Class2T = "0" + Class2H + ":0" + Class2M;
+                } else {
+                    if (Class2H < 10 && Class2M > 9) {
+                        Class2T = "0" + Class2H + ":" + Class2M;
+                    } if (Class2H > 9 && Class2M < 10) {
+                        Class2T = Class2H + ":0" + Class2M;
+                    } else {
+                        Class2T = Class2H + ":" + Class2M;
+                    }
+                }
+
+                var content = '<option value="' + Class2T + '">' + Class2T + '</option>';
+                $(content).appendTo($("#select-choice-b"));
+
+            }
+            $("#select-choice-b").selectmenu("refresh");
+
+            for (var y = 0; y < (360 / Class3) ; y++) {
+
+                if (Class3M + Class3 >= 120) {
+                    Class3H++;
+                    Class3M = Class3M + (Class3 - 60);
+                    do {
+                        Class3H++;
+                        Class3M = Class3M - 60;
+                    } while (Class3M >= 60)
+
+                } else if (Class3M + Class3 >= 60 && Class3M + Class3 < 120) {
+                    Class3H++;
+                    Class3M = Class3M + (Class3 - 60);
+                } else {
+                    Class3M = Class3M + Class3;
+                }
+
+                if (Class3H < 10 && Class3M < 10) {
+                    Class3T = "0" + Class3H + ":0" + Class3M;
+                } else {
+                    if (Class3H < 10 && Class3M > 9) {
+                        Class3T = "0" + Class3H + ":" + Class3M;
+                    } if (Class3H > 9 && Class3M < 10) {
+                        Class3T = Class3H + ":0" + Class3M;
+                    } else {
+                        Class3T = Class3H + ":" + Class3M;
+                    }
+                }
+
+                var content = '<option value="' + Class3T + '">' + Class3T + '</option>';
+                $(content).appendTo($("#select-choice-c"));
+
+            }
+            $("#select-choice-c").selectmenu("refresh");
+
+            for (var x = 0; x < (360 / Class4) ; x++) {
+
+                if (Class4M + Class4 >= 120) {
+                    Class4H++;
+                    Class4M = Class4M + (Class4 - 60);
+                    do {
+                        Class4H++;
+                        Class4M = Class4M - 60;
+                    } while (Class4M >= 60)
+
+                } else if (Class4M + Class4 >= 60 && Class4M + Class4 < 120) {
+                    Class4H++;
+                    Class4M = Class4M + (Class4 - 60);
+                } else {
+                    Class4M = Class4M + Class4;
+                }
+
+                if (Class4H < 10 && Class4M < 10) {
+                    Class4T = "0" + Class4H + ":0" + Class4M;
+                } else {
+                    if (Class4H < 10 && Class4M > 9) {
+                        Class4T = "0" + Class4H + ":" + Class4M;
+                    } if (Class4H > 9 && Class4M < 10) {
+                        Class4T = Class4H + ":0" + Class4M;
+                    } else {
+                        Class4T = Class4H + ":" + Class4M;
+                    }
+                }
+
+                var content = '<option value="' + Class4T + '">' + Class4T + '</option>';
+                $(content).appendTo($("#select-choice-d"));
+
+            }
+            $("#select-choice-d").selectmenu("refresh");
+
+
         }
     }
 }
