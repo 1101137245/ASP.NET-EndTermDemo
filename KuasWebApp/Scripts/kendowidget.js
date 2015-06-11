02ae7484418ea866e50dadbfefa9985f7dd808db
1101137245 <1101137245@gm.kuas.edu.tw>
2015-06-11 19:31:12 +0800

    customersearch by zhi

--- KuasWebApp/Scripts/kendowidget.js
+++ KuasWebApp/Scripts/kendowidget.js
@@ -100,4 +100,70 @@
         ]
     });
 
+    $("#Customersearchsubmit").click(function () {
+
+        var phone = $("#customerphone").val();
+        $("#grid2").kendoGrid({
+            dataSource: {
+
+                dataType: "jsonp",
+                transport: {
+                    read: {
+                        url: UrlBase + "api/Course/Name/" + phone,
+                        contentType: "application/json; charset=utf-8",
+                        type: "GET"
+                    },
+                    parameterMap: function (data, operation) {
+                        if (operation !== "read") {
+                            return kendo.stringify(data);
+                        }
+                    }
+                },
+                schema: {
+                    model: {
+                        id: "Id",
+                        fields: {
+                            Id: { type: "string", editable: true, nullable: false },
+                            Name: { type: "string", editable: true, nullable: false },
+                            Description: { type: "string", editable: true },
+                            Date: { type: "string", editable: true, nullable: false },
+                            Time: { type: "string", editable: true, nullable: false },
+                            Aboard: { type: "string", editable: true, nullable: false },
+                            Getoff: { type: "string", editable: true, nullable: false },
+                            Number: { type: "string", editable: true, nullable: false },
+                            Site: { type: "string", editable: true, nullable: false },
+                            Price: { type: "string", editable: true, nullable: false }
+                        }
+                    }
+                }
+
+            },
+            height: 400,
+            sortable: true,
+            scrollable: false,
+            sortable: true,
+            filterable: false,
+            pageable: false,
+            columns: [
+                { field: "Id", title: "編號", width: "5px" },
+                { field: "Name", title: "姓名", width: "40px" },
+                { field: "Date", title: "日期", width: "40px" },
+                { field: "Time", title: "時間", width: "40px" },
+                { field: "Aboard", title: "上車站", width: "40px" },
+                { field: "Getoff", title: "下車站", width: "40px" },
+                { field: "Number", title: "車次", width: "40px" },
+                { field: "Description", title: "數量", width: "40px" },
+                { field: "Site", title: "座位", width: "40px" },
+                { field: "Price", title: "價格", width: "40px" },
+
+
+            ]
+
+
+        });
+
+        //window.location = "http://localhost:1073/Views/Index.html#Customersearch";
+    });
+   
+
 });
